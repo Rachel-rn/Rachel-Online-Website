7074365152da44503020cdf57b319b9cdd9fd11a
Rachel-rn <njugunarachel213@gmail.com>
2025-11-18 23:25:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ menuToggle.addEventListener('click', () => {
 
 
 const typingText = document.getElementById('typing-text');
-const words = ["Science Communicator", "Tech & AI Enthusiast", "Career Growth Mentor"];
+const words = ["Science Communicator, Health Content Creator, Medical Writer"];
 let wordIndex = 0;
 let charIndex = 0;
 let deleting = false;
@@ -73,3 +73,4 @@ function sendEmail(event) {
     formMessage.style.color = "red";
   });
 }
+
